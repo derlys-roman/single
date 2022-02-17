35a220b8a35c1422a3a2e6acb6340d9afd9f4394
Derlys <derlysr6@gmail.com>
2022-02-17 06:53:43 -0300

    Add RegisterActivity for text view 'new here'

---
+++ app/src/main/java/com/example/single/RegisterActivity.kt
@@ -0,0 +1,15 @@
+package com.example.single
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.single.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
