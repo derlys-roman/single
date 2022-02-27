677718e04fef2db269a65f4b52686111db9ee318
Derlys <derlysr6@gmail.com>
2022-02-27 20:24:16 -0300

    connect layout components in RegisterActivity

--- app/src/main/java/com/example/single/RegisterActivity.kt
+++ app/src/main/java/com/example/single/RegisterActivity.kt
@@ -1,15 +1,30 @@
 package com.example.single
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import com.example.single.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterBinding
+    private lateinit var nameInput: EditText
+    private lateinit var emailInput: EditText
+    private lateinit var passwordInput: EditText
+    private lateinit var registerBtn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        fillComponents()
+    }
+
+    private fun fillComponents() {
+        this.nameInput = binding.nameInput
+        this.emailInput = binding.emailInput
+        this.passwordInput = binding.passinput
+        this.registerBtn = binding.regButton
     }
 }
