b179a9555a3f9c32aa2301aee106636040447bea
Derlys <derlysr6@gmail.com>
2022-03-18 06:34:59 -0300

    check userName and save photo in firestore

--- app/src/main/java/com/example/single/RegisterActivity.kt
+++ app/src/main/java/com/example/single/RegisterActivity.kt
@@ -1,5 +1,6 @@
 package com.example.single
 
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -12,7 +13,9 @@ import com.example.single.databinding.ActivityRegisterBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import de.hdodenhof.circleimageview.CircleImageView
+import java.util.*
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -24,9 +27,11 @@ class RegisterActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private lateinit var photo: Button
     private lateinit var frontPhoto: CircleImageView
+    private lateinit var mImageUri: Uri
     private val getImage = registerForActivityResult(
         ActivityResultContracts.GetContent(),
-        ActivityResultCallback { frontPhoto.setImageURI(it) }
+        ActivityResultCallback { mImageUri = it
+            frontPhoto.setImageURI(mImageUri)}
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,12 +57,13 @@ class RegisterActivity : AppCompatActivity() {
         val userName = nameInput.text.toString()
         val userEmail = emailInput.text.toString()
         val userPass = passwordInput.text.toString()
-        userCheck(userEmail, userPass)
+        userCheck(userEmail, userPass, userName)
         auth.createUserWithEmailAndPassword(userEmail, userPass)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
                     Log.d("Test", "createUserWithEmail:success")
+                    saveProfileFirebase()
                 } else {
                     // If sign in fails, display a message to the user.
                     Log.w("Test", "createUserWithEmail:failure", task.exception)
@@ -68,8 +74,14 @@ class RegisterActivity : AppCompatActivity() {
 
     }
 
-    private fun userCheck(userEmail: String, userPass: String) {
-        if (userEmail.isEmpty() || userPass.isEmpty()){
+    private fun saveProfileFirebase() {
+        val fileName = UUID.randomUUID().toString()
+        val ref = FirebaseStorage.getInstance().getReference("/images/$fileName")
+        ref.putFile(mImageUri)
+    }
+
+    private fun userCheck(userEmail: String, userPass: String, userName: String) {
+        if (userEmail.isEmpty() || userPass.isEmpty() || userName.isEmpty()){
             return Toast.makeText(
                 this,
                 "Os campos senha e email devem ser preenchidos",
